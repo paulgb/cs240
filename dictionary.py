a65f655b77b6e92f66547da08d6f6c07b1d06abf
Paul Butler <paul@wildcard.(none)>
2009-12-13 22:14:17 -0500

    fixed removal bug, added DoubleHashingDictionary

--- dictionary.py
+++ dictionary.py
@@ -27,6 +27,42 @@ False
 >>> d.get(11)
 'Baz'
 
+>>> d = DoubleHashingDictionary(8)
+>>> d.insert(8, 'A')
+>>> d.insert(7, 'E')
+>>> d.insert(6, 'I')
+>>> d.insert(12, 'O')
+>>> d.insert(4, 'U')
+>>> d.insert(16, 'M')
+>>> d.insert(2, 'N')
+>>> d.insert(10, 'Q')
+>>> sorted(d.keys())
+[2, 4, 6, 7, 8, 10, 12, 16]
+
+>>> d = DoubleHashingDictionary(13, 7)
+>>> d.insert(18, 0)
+>>> d.insert(41, 0)
+>>> d.insert(22, 0)
+>>> d.insert(44, 0)
+>>> d.insert(59, 0)
+>>> d.insert(32, 0)
+>>> d.insert(31, 0)
+>>> d.insert(73, 0)
+>>> d._table
+[(31, 0), None, (41, 0), None, None, (18, 0), (32, 0), (59, 0), (73, 0), (22, 0), (44, 0), None, None]
+
+>>> d = Dictionary(5)
+>>> d.insert(6, 0)
+>>> d.insert(3, 0)
+>>> d.insert(9, 0)
+>>> d.insert(2, 0)
+>>> d.insert(10, 0)
+>>> try: d.insert(11, 0)
+... except OverflowError: print 'ok'
+ok
+>>> d.remove(2)
+>>> d.insert(2, 0)
+
 '''
 
 DEFAULT_SIZE = 17
@@ -42,12 +78,12 @@ class Dictionary(object):
         return hash(key) % self._size
 
     def _rehash(self, hash, key):
-        return hash + 1 % self._size
+        return (hash + 1) % self._size
 
     def insert(self, key, value):
         hash = self._hashfun(key)
         for i in xrange(0, self._size):
-            if self._table[hash] is None:
+            if self._table[hash] in [SLOT_EMPTY, SLOT_REMOVED]:
                 self._table[hash] = (key, value)
                 return
             hash = self._rehash(hash, key)
@@ -101,4 +137,15 @@ class Dictionary(object):
                 key, value = slot
                 values.append(value)
         return values
+
+class DoubleHashingDictionary(Dictionary):
+    def __init__(self, size=DEFAULT_SIZE, q=17):
+        self._size = size
+        self._table = [SLOT_EMPTY] * size
+        self._q = q
+
+    def _rehash(self, hash, key):
+        q = self._q
+        d = lambda k: (q - k) % q
+        return (hash + d(key)) % self._size
 
