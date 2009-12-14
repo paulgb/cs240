0b310c00d04de11b36f3e95dd089f50998c15d0b
Paul Butler <paul@wildcard.(none)>
2009-12-13 21:16:49 -0500

    added heapify

--- heap.py
+++ heap.py
@@ -1,6 +1,8 @@
 
 
-r'''
+r"""
+
+# Try constructing a heap and popping its elements
 
 >>> h = Heap()
 >>> h.add(5, "Foo")
@@ -8,6 +10,10 @@ r'''
 (5, 'Foo')
 >>> h.empty()
 True
+
+# Intertwining insertions and deletions
+
+>>> h = Heap()
 >>> h.add(7, "Bar")
 >>> h.add(15, "Baz")
 >>> h.pop()
@@ -35,10 +41,63 @@ True
 >>> h.pop()
 (15, 'Baz')
 
-'''
+# Heapifying input
+
+>>> h = Heap([(5,5),(9,9),(2,2),(8,8),(4,4),(6,6),(3,3),(1,1)])
+>>> h.pop()
+(1, 1)
+>>> h.pop()
+(2, 2)
+>>> h.pop()
+(3, 3)
+>>> h.pop()
+(4, 4)
+>>> h.pop()
+(5, 5)
+>>> h.pop()
+(6, 6)
+>>> h.pop()
+(8, 8)
+>>> h.pop()
+(9, 9)
+
+"""
 
 class Heap(object):
     _array = list([None])
+
+    def _swap(self, index):
+        parent_index = index / 2
+        (self._array[index], self._array[parent_index]) = \
+                (self._array[parent_index], self._array[index])
+
+    def __init__(self, initial=None):
+        if initial is None:
+            self._array = [None]
+        else:
+            self._array = [None] + initial
+            for i in xrange(len(initial) / 2, 0, -1):
+                self._heapify(i)
+
+    def _heapify(self, index):
+        if index > len(self._array) / 2:
+            return
+        left_child_index = index * 2
+        right_child_index = index * 2 + 1
+        if right_child_index < len(self._array):
+            (min_value, min_index) = min(
+                    (self._array[left_child_index][0],
+                        left_child_index),
+                    (self._array[right_child_index][0],
+                        right_child_index))
+            if min_value < self._array[index][0]:
+                self._swap(min_index)
+                self._heapify(min_index)
+        elif left_child_index < len(self._array):
+            if self._array[left_child_index][0] < \
+                    self._array[index][0]:
+                self._swap(left_child_index)
+                self._heapify(left_child_index)
 
     def add(self, key, value):
         self._array.append((key, value))
@@ -49,8 +108,7 @@ class Heap(object):
         if parent_index == 0:
             return
         if self._array[parent_index][0] > self._array[index][0]:
-            (self._array[parent_index], self._array[index]) = \
-                    (self._array[index], self._array[parent_index])
+            self._swap(index)
             self._upheap(parent_index)
 
     def pop(self):
@@ -66,16 +124,20 @@ class Heap(object):
     def _downheap(self, index):
         left_child_index = index * 2
         right_child_index = index * 2 + 1
-        if left_child_index < len(self._array) and \
-                self._array[left_child_index] < self._array[index]:
-            (self._array[left_child_index], self._array[index]) = \
-                    (self._array[index], self._array[left_child_index])
-            self._downheap(left_child_index)
-        elif right_child_index < len(self._array) and \
-                self._array[right_child_index] < self._array[index]:
-            (self._array[right_child_index], self._array[index]) = \
-                    (self._array[index], self._array[right_child_index])
-            self._downheap(right_child_index)
+        if right_child_index < len(self._array):
+            (min_value, min_index) = min(
+                    (self._array[left_child_index][0],
+                        left_child_index),
+                    (self._array[right_child_index][0],
+                        right_child_index))
+            if min_value < self._array[index]:
+                self._swap(min_index)
+                self._downheap(min_index)
+        else:
+            if left_child_index < len(self._array):
+                if self._array[left_child_index][0] < \
+                        self._array[index][0]:
+                    self._swap(left_child_index)
 
     def size(self):
         pass
